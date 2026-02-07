e8d64e0a2aba1a7c600436a7aed6a5a139e9f117
yamunaparameshwar <yamunaparameshwar1325@gmail.com>
2026-02-07 10:49:22 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[]args ) {
+        int a=10;
+        int b=0xAA;
+        int c=055;
+        int d=0b1010;
+        System.out.println(a);
+    }
+    
+}
