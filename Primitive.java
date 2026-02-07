49e295dbdeac2395210e15dbfd36c27640844dbd
yamunaparameshwar <yamunaparameshwar1325@gmail.com>
2026-02-07 10:47:09 +0530

    Add files via upload

---
+++ Primitive.java
@@ -0,0 +1,14 @@
+public class Primitive {
+    public static void main(String[] args)
+    {
+       String name="yamuna";
+       int age=20;
+       int marks=955;
+       boolean status= true;
+       System.out.println(name);
+    System.out.println(age);
+       System.out.println(marks);
+       System.out.println(status);
+    
+}
+}
