85df5a8ee30ba0d980d6e2045bb2ec3f7707a351
yamunaparameshwar <yamunaparameshwar1325@gmail.com>
2026-02-07 11:33:54 +0530

    Add files via upload

---
+++ Ticket.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Ticket {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter movie");
+        String movie=sc.next();
+        System.out.println("enter seatnum");
+        int seatnum=sc.nextInt();
+        System.out.println("enter tktprice");
+        int tktprice=sc.nextInt();
+        System.out.println("enter book");
+        boolean book=sc.nextBoolean();
+
+
+    }
+    
+}
